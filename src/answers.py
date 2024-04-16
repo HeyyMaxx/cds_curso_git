022997a5532bc4157247cfa8f8244955724afd70
Marcus Meira <marcusvmeirap@gmail.com>
2024-04-16 12:57:08 -0300

    cut the button that was not working out of the project

--- src/answers.py
+++ src/answers.py
@@ -247,30 +247,4 @@ def rd3_question_7(df):
 
     st.dataframe(df_selected)
 
-    df_xlsx = to_excel(df_selected)
-
-    st.download_button(
-        label="📥 Download Purchase Suggestions",
-        data=df_xlsx,
-        file_name="purchase_suggestions.xlsx",
-    )
-
     return None
-
-
-def to_excel(df):
-    output = BytesIO()
-
-    writer = pd.ExcelWriter(output, engine="xlsxwriter")
-
-    df.to_excel(writer, index=False, sheet_name="Sheet1")
-
-    worksheet = writer.sheets["Sheet1"]
-
-    worksheet.set_column("A:A", None)
-
-    writer.save()
-
-    processed_data = output.getvalue()
-
-    return processed_data
